bbe005b8b2676e79b15781b38776bba2f5f0522d
Daggron <returnofking04@gmail.com>
2019-11-15 23:49:04 +0530

    Added cards view

--- src/components/Category/title/data.jsx
+++ src/components/Category/title/data.jsx
@@ -1,5 +1,35 @@
 import React from 'react'
 import axios from 'axios';
+import './title.css'
+
+const Cards = ({name,icon})=>{
+
+    return(
+        <div className="wrapper">
+
+        
+            <div class="card">
+                <div class="card-body">
+                    <h5 className="card-title"><i className={icon}></i>&nbsp;{name}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    {/* <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a> */}
+                </div>
+            </div>
+
+        </div>
+
+        // <div className="cards">
+                                        
+        // <h1>
+        // <i className={icon}></i>&nbsp;
+        //     {
+        //         name
+        //     }
+        // </h1>
+        // </div>
+    )
+}
 
 class Data extends React.Component{
     constructor(props){
@@ -23,19 +53,11 @@ class Data extends React.Component{
     render(){
         return(
             <>
-                <div className="mx-auto">
+                <div className="ml-5 my-5">
                     {
                         this.state.categories.map(category=>{
                             return(
-                                <div className="cards" key={category.name}>
-                                    
-                                    <h1>
-                                    <i className={category.icon}></i>&nbsp;
-                                        {
-                                        category.name
-                                        }
-                                    </h1>
-                                </div>
+                               <Cards key={category.name} name={category.name} icon={category.icon} /> 
                             )
                         })
                     }
